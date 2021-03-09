5eda59ad29602b75623bb7485eae034790d0cb5d
Kkasya <kasya--k@yandex.by>
2021-03-09 17:46:24 +0300

    feat: add item-card

--- travel-app/src/components/item-card/item-card.js
+++ travel-app/src/components/item-card/item-card.js
@@ -1,7 +1,22 @@
-const ItemCard = () => {
-  return(
-    <div >Item card</div>
-  )
+import React from 'react';
+import ATTRACTIONS from '../../data/ATTRACTIONS.json';
+
+const ItemCard = ({id}) => {
+	const itemCardImage = {
+		background: `url('/images/${id}/main.jpg')`,
+		backgroundSize: 'cover'
+	};
+
+	return (
+		<div style={itemCardImage} className="item-card" >
+			<div className="item-card item-card__content" >
+				<div className="item-card_infoblock" >
+					<h3 >{ATTRACTIONS[id].title}</h3 >
+					<p className="item-card_infoblock__capital" >{ATTRACTIONS[id].capital}</p >
+				</div >
+			</div >
+		</div >
+	)
 };
 
 export default ItemCard;
