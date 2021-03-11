3a8131bb78a83c659a5179ff99253b0b2964e029
Anastassia Kozlovskaya <52949341+Kkasya@users.noreply.github.com>
2021-03-11 08:31:03 +0300

    fix: change classes for footer itemGithubMembers

--- travel-app/src/components/footer/itemGithubMembers.js
+++ travel-app/src/components/footer/itemGithubMembers.js
@@ -5,12 +5,12 @@ const ItemGithubMembers = ({name, endLink}) => {
 	const src = `${githubSrc}${endLink}`;
 
 	return (
-		<div className="footer__content__github-members__item" >
+		<div className="footer__github-members--item" >
 			<a target='_blank' rel="noreferrer" href={src} >
-				<img className="footer__content__img__git"
+				<img className="footer__img--git"
 						 src="/images/github.svg"
 						 alt="github" />
-				<span >{name}</span >
+				<span className="aa" >{name}</span >
 			</a >
 		</div >
 	)
