5abfabf9666734e512f8048488bb79a7c771f273
Anastassia Kozlovskaya <kasya--k@yandex.ru>
2021-03-11 10:28:22 +0300

    feat: add header with adaptive

--- travel-app/src/components/app/app.js
+++ travel-app/src/components/app/app.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ItemCard from "../item-card";
 
 const App = () => {
   return(
     <div className="app">
       <div className="app__title">Travel app</div>
-        <ItemCard id='0' />
     </div>
   )
 };
--- travel-app/src/components/header/header.js
+++ travel-app/src/components/header/header.js
@@ -1,7 +1,20 @@
+import React from 'react';
+import Search from "../search";
+import Lang from "../lang";
+
 const Header = () => {
-  return(
-    <header className="header">header</header>
-  )
+	return (
+		<header className="header" >
+			<div className="header__content" >
+				<a className="header__logo" href="#" >
+					<img src="/images/icon-travel.png" alt="travel-app" ></img >
+					<span >travelapp</span >
+				</a >
+				<Search />
+				<Lang />
+			</div >
+		</header >
+	)
 };
 
 export default Header;
