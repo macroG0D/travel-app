18f67d3d645d61d5cf1b1bcceefaec639cc1ac93
Heliken <burshf@gmail.com>
2021-03-13 17:42:36 +0300

    feat: add header search component

--- travel-app/src/components/app/app.js
+++ travel-app/src/components/app/app.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from "../header";
 import Footer from "../footer";
 import Main from "../../pages/main";
 
 const App = () => {
+  const [filterVal, setFilterVal] = useState('');
+
   return(
     <div className="app">
-        <Header isMain="true" />
-        <Main />
+        <Header isMain="true" updateFilter={setFilterVal}/>
+        <Main filterVal={filterVal}/>
         <Footer />
     </div>
   )
