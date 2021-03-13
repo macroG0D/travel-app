7063d26ca2a6606721a0680807b3c7a0e25d5e3f
Kkasya <kasya--k@yandex.by>
2021-03-13 09:32:40 +0300

    feat: add gallery

---
+++ travel-app/src/components/gallery/gallery.js
@@ -0,0 +1,32 @@
+import React, {useContext} from 'react';
+import ImageGallery from "react-image-gallery/";
+import Context from "../context";
+import {ATTRACTIONSDE, ATTRACTIONSEN, ATTRACTIONSRU} from "../../data";
+
+const Gallery = ({id}) => {
+  const [lang] = useContext(Context);
+  const data = (lang === 'en') ? ATTRACTIONSEN : (lang === 'ru') ? ATTRACTIONSRU : ATTRACTIONSDE;
+  const infoGallery  = data[id]['info-gallery'];
+  const srcImage = `/images/${id}`;
+  console.log(infoGallery);
+
+
+  const images = infoGallery.map(({name, description}, ind) => {
+    console.log(`${srcImage}/${ind}.jpg`)
+    return {
+      thumbnail: `${srcImage}/${ind}.jpg`,
+      original: `${srcImage}/${ind}.jpg`,
+      // description: description,
+      originalAlt:name,
+      thumbnailAlt:name,
+    }
+  })
+
+  return <ImageGallery
+    items={images}
+    thumbnailPosition='right'
+    showPlayButton={false}
+  />;
+};
+
+export default Gallery;
